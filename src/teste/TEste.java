f5988343ea79bc680a7b4a5f9c2c8c886bc8d4ab
thalissonc <thalissontan13@gmail.com>
2021-07-06 11:38:10 -0300

    Initial commit

---
+++ src/teste/TEste.java
@@ -0,0 +1,118 @@
+package teste;
+
+class Teste {
+
+    public static void main(String[] args) {
+        // ("sout + TAB" para "System.out.printl0n")
+        System.out.println("Vamo ver se essa ***** roda:");
+        
+        
+        //(-int- tipo de variavel. Exemplos:)
+        //int dataValue;
+        //dataValue = 100;
+        //int myInfo = 200;
+        
+        //int 2much; ( nomes de variaveis n podem começar com numeros)
+        
+        //int sum;
+        //int studentCount;
+        //int bankAccountBalance;
+        //int level2Trainnig;       
+        
+        
+        //int myVar;
+        //myVar = 50;
+        //System.out.println(myVar);
+        //int anotherVar  = 100;
+        //System.out.println(anotherVar);
+        //myVar = anotherVar;
+        //System.out.println(myVar);
+        
+        
+       
+        //final int maxStudents = 25;
+        //final int someVariable;
+        //int someOtherVariable = 100;
+        //someVariable = someOtherVariable;
+        //System.out.println(maxStudents);
+        //System.out.println(someOtherVariable);
+        
+        
+        //        //(varaiveis tem tamanho maximo, por isso existem varios tipos como:) 
+        //        byte numberOfEnglishLetters = 26;
+        //        short feetInAMile = 5280;
+        //        int milesToSun = 92960000;
+        //        long milesInALightYear = 5879000000000L;      // long necessita de um "L" no final.
+        //        float killometersInAMarathon = 42.195f;       // float suporta numeros com pontos e necessita de "f" no final     .   
+        //        double absoluteZeroInCelsius = 0.0000000001d;
+        //        char regularU = 'u';                          // variavel que suporta caracteres.
+        //        char accentedU = '\u00DA';                    // Ú
+        //        boolean iLoveJava = true;                     // boolean suporta 'true' ou 'false' apenas.
+        
+        //        int firstValue = 100;
+        //        int otherValue = firstValue;
+        //        firstValue = 50;
+        //        otherValue = 700;
+        
+        
+        //        int someValue = 5;                     // "++" indica ao codigo para somar +1 ao valor
+        //        System.out.println(++someValue);       // quando o ++ vem antes da variavel, é somado antes de ser exibido, exibindo o valor ja somado
+        //        System.out.println(someValue);         
+        //        
+        //        int someOtherValue = 5;
+        //        System.out.println(someOtherValue++);  // quando o ++ vem depois da variavel, o valor é somado depois de ser exibido, assim o valor exibido não é com a soma.
+        //        System.out.println(someOtherValue);        
+        
+        
+        //        int myValue = 50;
+        //        myValue -=5;                           // - antes de = realiza subtração
+        //        System.out.println(myValue);
+        
+        
+        //        int myOtherValue = 100;
+        //        int val1 = 5;
+        //        int val2 = 10;
+        //        myOtherValue /= val1 * val2;
+        //        System.out.println(myOtherValue);
+        
+        //        int intValueOne = 50;
+        //        long longValueOne = intValueOne;      // uma variavel int pode "caber" em variavel long
+        //        
+        //        long longValueTwo = 50;               // mas uma variavel long não "cabe" em uma variavel int
+        //        int intValueTwo = (int) longValueTwo; // para fazer isso é necessario "converter" o tipo de variavel long para int, usando "( )"
+
+
+        //        int value1 = 7;
+        //        int value2 = 5;
+        //        int maxValue = value1 > value2 ? value1 : value2;
+        //        System.out.println(maxValue);
+
+        //        int value1 = 10;
+        //        int value2 = 10;
+        //        if (value1 > value2)
+        //            System.out.println("É maior");
+        //        else
+        //            System.out.println("É menor");
+        //        if (value1 == value2)
+        //            System.out.println("É igual");
+
+
+
+        //        int a = 20, b = 14, c = 5;
+        //        if ( a > b & b > c )  //para usar "ou" use "|"
+        //            System.out.println("A é maior que C");
+
+        int v1 = 4, v2 = 12;
+        final int diff;
+        if (v1 > v2 ) {  // para o java executar varias linhas como um comando usa-se "{ }"
+            diff = v1 - v2;
+            System.out.println("V1 é maior que V2, diferença = " + diff);
+        }
+        else {
+            diff = v2 - v1;
+            System.out.println("V1 é menor que V2, diferença = " + diff);
+        }
+
+    }
+    
+}
