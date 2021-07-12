d58df55eab4450f2e3d91e461a8099432095e11c
LipeNebrosa <87019102+LipeNebrosa@users.noreply.github.com>
2021-07-12 09:56:49 -0300

    QUESTÃO5

---
+++ src/Atividade/Atividade.java
@@ -0,0 +1,27 @@
+package Atividade;
+
+public class Atividade {
+
+    public static void main(String[] args) {
+
+        //    1    System.out.println("alo mundo de merda, kkk q odio");
+
+        //    2   int numero = 69;
+        //        System.out.println("O numero informado foi : " + numero);
+
+        //    3   int num1 = 25;
+        //        int num2 = 50;
+        //        System.out.println(num1 + num2);
+
+        //    4   int nota1 = 10;
+        //        int nota2 = 8;
+        //        int nota3 = 9;
+        //        int nota4 = 5;
+        //        System.out.println((nota1 + nota2 + nota3 + nota4) /4);
+
+        float metros = 2.67f;
+        System.out.println("Essa medida em centimetros é : " + metros*100 );
+
+
+    }
+}
