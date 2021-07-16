698e66249b8e7df63529f122223a98e85ae012d8
LipeNebrosa <87019102+LipeNebrosa@users.noreply.github.com>
2021-07-16 10:56:23 -0300

    lista3 quest1

---
+++ src/Lista2/Questao11.java
@@ -0,0 +1,10 @@
+package Lista2;
+
+import java.util.Scanner;
+
+public class Questao11 {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/Lista3/Questao1.java
@@ -0,0 +1,32 @@
+package Lista3;
+
+import jdk.nashorn.internal.ir.WhileNode;
+
+import java.util.Scanner;
+
+public class Questao1 {
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Insira uma nota: (0 - 10)");
+        int nota = scan.nextInt();
+
+
+        while (nota < 0 || nota > 10 ){
+            System.out.println(" Nota invalida, repita:");
+            nota = scan.nextInt();
+        }
+
+
+        System.out.println(" Obrigado pela avaliação!");
+
+
+
+
+
+
+
+
+    }
+}
